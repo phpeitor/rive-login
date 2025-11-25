8c2c03ad2f590494100da4123de5fcc5c03b7298
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-11-25 18:24:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+// importa tu login animado
+import LoginFormComponent from './login-form/LoginFormComponent';
+import './login-form/LoginFormComponent.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginFormComponent />
+    </div>
+  );
+}
+
+export default App;
